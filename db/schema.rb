a3d15bafb9f5f7f22f1fe6481134ba9b5c46967e
jin3333 <61690420+jin3333@users.noreply.github.com>
2020-06-05 16:17:04 +0900

    商品出品マークアップ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200327123104) do
+ActiveRecord::Schema.define(version: 20200605071240) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -44,16 +44,7 @@ ActiveRecord::Schema.define(version: 20200327123104) do
     t.datetime "updated_at",                null: false
     t.integer  "user_id"
     t.integer  "likes_count"
-  end
-
-  create_table "relationships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.integer  "follow_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["follow_id"], name: "index_relationships_on_follow_id", using: :btree
-    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true, using: :btree
-    t.index ["user_id"], name: "index_relationships_on_user_id", using: :btree
+    t.string   "name"
   end
 
   create_table "taggings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -96,7 +87,5 @@ ActiveRecord::Schema.define(version: 20200327123104) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "relationships", "users"
-  add_foreign_key "relationships", "users", column: "follow_id"
   add_foreign_key "taggings", "tags"
 end
